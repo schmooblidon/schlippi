4fba8e599e8f734ca0466ed5e92a1f9b0e4fee08
schmooblidon <willzyx123@googlemail.com>
2020-07-23 22:05:02 +0100

    fixed null metadata files, optional character limiter, fixed special characters (giga etc), added live progress console log

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // GRAB EXTERNAL STUFF WE NEED
-const slp = require('slp-parser-js');
+const slp = require('@slippi/slippi-js');
 const SlippiGame = slp.default;
 const fs = require('fs');
 const path = require('path');
@@ -18,6 +18,11 @@ const minimumGameTime = 1200; // 20 seconds
 const captureTimePreInput = 600; // 10 seconds
 const captureTimePostInput = 60; // 1 second
 const captureLockoutTime = 300; // 5 seconds
+
+const limitCharacters = false;
+
+// possible characters: "Captain Falcon", "Donkey Kong", "Fox", "Mr. Game & Watch", "Kirby", "Bowser", "Link", "Luigi", "Mario", "Marth", "Mewtwo", "Ness", "Peach", "Pikachu", "Ice Climbers", "Jigglypuff", "Samus", "Yoshi", "Zelda", "Sheik", "Falco", "Young Link", "Dr. Mario", "Roy", "Pichu", "Ganondorf"
+const acceptableCharacters = ["Fox"];
 
 // the input it looks for to find moments. 
 // possible inputs: "A", "B", "X", "Y", "Z", "L", "R", "Start", "DpadRight", "DpadLeft", "DpadUp", "DpadDown"
@@ -55,11 +60,47 @@ function GetCharName (players, p) {
   var char_name = "";
   for (var m=0;m<players.length;m++) {
     if (players[m].playerIndex == p) {
-      char_name = slp.characters.getCharacterName(players[m].characterId);
+      if (players[m].characterId > 25) {
+        char_name = "Special";
+      }
+      else {
+        char_name = slp.characters.getCharacterName(players[m].characterId);
+      }
       break;
     }
   }
   return char_name;
+}
+
+function CharIsAcceptable(players, p) {
+  if (!limitCharacters) return true;
+  return _CharIsAcceptable(GetCharName(players, p));
+}
+
+function _CharIsAcceptable(character) {
+  for (var i=0;i<acceptableCharacters.length;i++) {
+    if (acceptableCharacters[i] == character) return true;
+  }
+  return false;
+}
+
+function GetLastFrame(game) {
+  var lf = game.getLatestFrame();
+  if (lf == null) return null;
+  if (lf == undefined) return null;
+  return lf.frame;
+}
+
+function logProgress(count, total){
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(count + '/' + total);
+}
+
+function logMessage(message) {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  console.log(message);
 }
 
 function GetTimeText(n) {
@@ -125,8 +166,12 @@ var output = {
 
 var momentCount = 0;
 
+logProgress(0, files.length);
+
 // for each file
 for (var i=0;i<files.length;i++) {
+
+  logProgress(i+1, files.length);
 
   // grab parsed game object from slp-parser-js
   const game = new SlippiGame(files[i]);
@@ -147,24 +192,34 @@ for (var i=0;i<files.length;i++) {
     if (skip) continue;
   }
 
-  const metadata = game.getMetadata();
-
-  if (metadata.lastFrame === undefined) continue;
-  // if game is too short
-  if (metadata.lastFrame < minimumGameTime) continue;
-
-  if (!game.getGameEnd()) {
-    console.log("Bad file! Did the game crash?");
+  const lastframe = GetLastFrame(game);
+  if (lastframe == null) {
+    logMessage("Bad file!");
     continue;
   }
 
+  /*if (!game.getGameEnd()) {
+    console.log("Bad file! Did the game crash?");
+    continue;
+  }*/
+
   const frames = game.getFrames();
+
+  if (frames == null) {
+    logMessage("Bad file!");
+    continue;
+  }
+
+  // if game is too short
+  if (lastframe < minimumGameTime) continue;
 
   // reset lockout
   var lockout = 0;
 
   // for each frame in the game
-  for (var n=-123;n<metadata.lastFrame;n++) {
+  for (var n=-123;n<lastframe;n++) {
+
+    if (frames[n] == null) break;
 
     // if currently locked out, skip test
     if (lockout > 0) {
@@ -196,33 +251,43 @@ for (var i=0;i<files.length;i++) {
             if (i > 0) {
               const prev_game = new SlippiGame(files[i-1]);
               const prev_settings = prev_game.getSettings();
-              const prev_metadata = prev_game.getMetadata();
-
-              output.queue[momentCount] = {
-                "path" : files[i-1],
-                "startFrame" : Math.max(-123, prev_metadata.lastFrame-captureTimePreInput),
-                "endFrame" : prev_metadata.lastFrame
+              const prev_lastframe = GetLastFrame(prev_game);
+              if (prev_lastframe == null) {
+                logMessage("Found Ender with bad file!");
+                continue;
               }
 
-              lockout = captureLockoutTime;
-              momentCount++;
+              if (CharIsAcceptable(prev_settings.players, p)) {
 
-              console.log(`[MOMENT #${momentCount.toString().padStart(1, '0')}] Port ${p+1} | ${GetCharName(prev_settings.players, p)} | ${slp.stages.getStageName(prev_settings.stageId)} | ${GetTimeText(prev_metadata.lastFrame)} into game | ENDER!`);
+                output.queue[momentCount] = {
+                  "path" : files[i-1],
+                  "startFrame" : Math.max(-123, prev_lastframe-captureTimePreInput),
+                  "endFrame" : prev_lastframe
+                }
+
+                lockout = captureLockoutTime;
+                momentCount++;
+
+                logMessage(`[MOMENT #${momentCount.toString().padStart(1, '0')}] Port ${p+1} | ${GetCharName(prev_settings.players, p)} | ${slp.stages.getStageName(prev_settings.stageId)} | ${GetTimeText(prev_lastframe)} into game | ENDER!`);
+              }
 
             }
           }
           else {
-            // FOUND MOMENT!
-            output.queue[momentCount] = {
-              "path" : files[i],
-              "startFrame" : Math.max(-123, n-captureTimePreInput),
-              "endFrame" : Math.min(metadata.lastFrame, n+captureTimePostInput)
+
+            if (CharIsAcceptable(settings.players, p)) {
+              // FOUND MOMENT!
+              output.queue[momentCount] = {
+                "path" : files[i],
+                "startFrame" : Math.max(-123, n-captureTimePreInput),
+                "endFrame" : Math.min(lastframe, n+captureTimePostInput)
+              }
+              
+              lockout = captureLockoutTime;
+              momentCount++;
+              
+              logMessage(`[MOMENT #${momentCount.toString().padStart(1, '0')}] Port ${p+1} | ${GetCharName(settings.players, p)} | ${slp.stages.getStageName(settings.stageId)} | ${GetTimeText(n)} into game`);
             }
-            
-            lockout = captureLockoutTime;
-            momentCount++;
-            
-            console.log(`[MOMENT #${momentCount.toString().padStart(1, '0')}] Port ${p+1} | ${GetCharName(settings.players, p)} | ${slp.stages.getStageName(settings.stageId)} | ${GetTimeText(n)} into game`);
           }
         }
       }
@@ -230,6 +295,7 @@ for (var i=0;i<files.length;i++) {
   }
 }
 
+console.log("\n");
 console.log(`Found ${momentCount} moments!`);
 
 // now write the output to a json file
